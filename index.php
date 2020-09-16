3967439b87ecb5bf96e7dca15c109d6c3b1738a7
teteh68 <63129546+teteh68@users.noreply.github.com>
2020-09-16 07:04:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lvcyd</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="js/script.js">
+    </head>
+    <body>
+        <nav class="navbar">
+
+            <h3 class="logo">Logo</h3>
+
+            <div class="icon"><i class="fas fa-bars"></i></div>
+
+            <div class="links-wrapper active">
+            
+                <div class="backdrop"></div>
+                <div class="close-btn"><i class="fas fa-times"></i></div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+
+            </div>
+
+        </nav>
+    </body>
+</html>
